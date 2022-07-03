f76d2e7c5f857493e5ef5e341b88d900493415b1
Priyansh Dwivedi <priyansh28dwivedi@gmail.com>
2022-07-03 17:32:56 +0530

    second commit

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Gdrive from './googledrivepicker'
-
+import Dropbox from './dropbox'
 import { Button, Typography } from '@material-ui/core'
 
 const DefaultButton = (props) => {
   return (
      <div>
       <Gdrive />
-     
+      <Dropbox />
       <a href='#' class="dropbox"> 
       <i class="icon" ></i> 
       "dropbox"
